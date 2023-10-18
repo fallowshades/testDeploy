2bfc337e647e5e21307ef1bc04bbd8567dd29714
fallowShades <fallowshades@gmail.com>
2023-10-18 15:42:03 +0200

    2.2 Error page
    https://onedrive.live.com/view.aspx?resid=73D8324D775D536C%215116&id=documents&wd=target%28Attempt%20again.one%7C3E2DDDF7-4AFC-494E-913E-F86904E58590%2F%281%5C%291st%20layer%7C7003D77D-8E4C-4E38-A4A0-64C8BE135711%2F%29
    onenote:https://d.docs.live.net/73d8324d775d536c/Documents/Planning/Attempt%20again.one#(1)1st%20layer&section-id={3E2DDDF7-4AFC-494E-913E-F86904E58590}&page-id={7003D77D-8E4C-4E38-A4A0-64C8BE135711}&end

--- Hamoria/src/pages/1stLayerPageAndAuth/Error.jsx
+++ Hamoria/src/pages/1stLayerPageAndAuth/Error.jsx
@@ -1,4 +1,11 @@
+import { Link, useRouteError } from 'react-router-dom'
 const Error = () => {
-  return <div>Error</div>
+  const error = useRouteError()
+  return (
+    <div>
+      <h1>Error Page!!!</h1>
+      <Link to="/dashboard"></Link>
+    </div>
+  )
 }
 export default Error
