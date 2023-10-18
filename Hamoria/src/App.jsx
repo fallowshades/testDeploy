3d60c6bd859676bb4face96976c264afaca97ff5
fallowShades <fallowshades@gmail.com>
2023-10-18 15:11:32 +0200

    3.Public pages
    https://onedrive.live.com/view.aspx?resid=73D8324D775D536C%215116&id=documents&wd=target%28Attempt%20again.one%7C3E2DDDF7-4AFC-494E-913E-F86904E58590%2F%281%5C%291st%20layer%7C7003D77D-8E4C-4E38-A4A0-64C8BE135711%2F%29
    onenote:https://d.docs.live.net/73d8324d775d536c/Documents/Planning/Attempt%20again.one#(1)1st%20layer&section-id={3E2DDDF7-4AFC-494E-913E-F86904E58590}&page-id={7003D77D-8E4C-4E38-A4A0-64C8BE135711}&end

--- Hamoria/src/App.jsx
+++ Hamoria/src/App.jsx
@@ -1,9 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import {
+  HomeLayout,
+  Error,
+  Landing,
+  Login,
+  Register,
+  RegisterInteriorPage,
+  LoginInteriorPage,
+} from './pages/1stLayerPageAndAuth'
+
+import {
+  AddAchievement,
+  Admin,
+  AllAchievements,
+  EditAchievement,
+  Profile,
+  Stats,
+} from './pages/2ndLayerRolePrivilages'
+
+import { About, DashboardLayout, FeautureHome } from './pages/2ndLayerShared'
+
+import { SignList, Signs, SingleSign } from './pages/2ndLayerStuff'
+
+import { Checkout, Orders } from './pages/2ndLayerUtils'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/dashboard',
+    children: [
+      {
+        index: true,
+        element: <FeautureHome />,
+      },
+      {
+        path: 'signs',
+        element: <Signs />,
+      },
+      {
+        path: 'signs/:id',
+        element: <SingleSign />,
+      },
+      {
+        path: 'sigtn-list',
+        element: <SignList />,
+      },
+      { path: 'about', element: <About /> },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/loginInterior',
+    element: <Login />,
+  },
+  {
+    path: '/registerInterior',
+    element: <Register />,
+  },
+])
 
 const App = () => {
-  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>
+  return <RouterProvider router={router} />
 }
 export default App
